6fd35a47dcf25c50f9b69bdb1ec70cfb85bd4c33
albshmhnd <albshmhndsbdalrhmnalfra@gmail.com>
2026-02-04 10:16:32 -0500

    task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,7 @@
+#task1
+#cyber security
+length = int(input("Enter the length rectangle\n"))
+width = int(input("Enter the width rectangle\n"))
+College = (length * width)
+print("Output "+str(College))
 
